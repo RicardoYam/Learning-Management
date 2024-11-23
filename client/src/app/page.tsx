019b2730eed75178f434cfbe907ff787707183b5
RicardoYam <10678784+ricardoyam@user.noreply.gitee.com>
2024-11-23 15:16:48 +1000

    update landing page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
 import NonDashboardNavbar from "@/src/components/NonDashboardNavbar";
+import Landing from "@/src/app/(nondashboard)/landing/page";
+import Footer from "@/src/components/Footer";
 
 export default function Home() {
   return (
     <div className="nondashboard-layout">
       <NonDashboardNavbar />
+      <main className="nondashboard-layout__main">
+        <Landing />
+      </main>
+      <Footer />
     </div>
   );
 }
